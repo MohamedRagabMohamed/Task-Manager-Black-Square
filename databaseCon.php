afcc89cb29a9444840e68e3559f9238e923d9a59
Your Name <mohamed225.mr@gmail.com>
2018-08-16 17:59:21 +0200

    update database name

--- databaseCon.php
+++ databaseCon.php
@@ -4,7 +4,7 @@
 $servername = "localhost";
 $username   = "root";
 $password   = "";
-$dbname     = "test";
+$dbname     = "black_square";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
